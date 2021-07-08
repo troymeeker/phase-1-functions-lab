50f6aa17609ccec71485966150da32f347408b1f
troymeeker <troymeeker1@gmail.com>
2021-07-07 21:49:31 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(din){
+  if(din>42){
+      return din-42;
+  }else if(din<42){
+      return 42-din;
+  }
+}
+
+distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(din){
+    return distanceFromHqInBlocks(din)*264;
+    
+}
+distanceFromHqInFeet(25);
+
+function distanceTravelledInFeet(start,end){
+    if(start>end){
+        return (start-end) * 264;
+    }else if(start<end){
+        return (end-start) * 264;
+    }
+    
+}
+distanceTravelledInFeet(10,5);
+
+
+function calculatesFarePrice(start, destination){
+  if(distanceTravelledInFeet<=400){
+        return 'Free sample.';
+    }else if(distanceTravelledInFeet>400 && distanceTravelledInFeet<2000){
+        return (distanceTravelledInFeet)*.02;
+    }else if((distanceTravelledInFeet-400)>2000){
+        return 'That will be $25';
+    } else if(distanceTravelledInFeet>2500){
+        return 'cannot travel that far';
+          } 
+}
+calculatesFarePrice(44,43);
